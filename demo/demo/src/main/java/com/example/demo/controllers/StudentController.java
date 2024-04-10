01e10a2b94326257ae2dce977003083f5ba3f383
dannyDv23 <datdvhe172079@fpt.edu.vn>
2024-04-10 11:30:52 +0700

    feat : CRUD Student backend

--- demo/demo/src/main/java/com/example/demo/controllers/StudentController.java
+++ demo/demo/src/main/java/com/example/demo/controllers/StudentController.java
@@ -2,25 +2,39 @@ package com.example.demo.controllers;
 
 import com.example.demo.model.Student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
     private final StudentService studentService;
-@Autowired
-    public StudentController(StudentService studentService) {
+
+    @Autowired
+    public StudentController (StudentService studentService){
         this.studentService = studentService;
     }
 
     @GetMapping
-    public List<Student> getStudents() {
-       return studentService.getStudents();
+    public List<Student> getStudent(){
+        return studentService.getStudent();
+    }
+
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId,
+                              @RequestParam(required = false) String name,
+                              @RequestParam(required = false) String email){
+        studentService.updateStudent(studentId, name, email);
     }
 }
--- demo/demo/src/main/java/com/example/demo/model/Student.java
+++ demo/demo/src/main/java/com/example/demo/model/Student.java
@@ -1,27 +1,44 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
-    private String eamil;
-    private LocalDate date;
+    private LocalDate dob;
+    @Transient
     private Integer age;
+    private String email;
 
-    public Student(Long id, String name, String eamil, LocalDate date, Integer age) {
-        this.id = id;
-        this.name = name;
-        this.eamil = eamil;
-        this.date = date;
-        this.age = age;
+    public  Student() {
     }
 
-    public Student( String name, String eamil, LocalDate date, Integer age) {
+    public Student(Long id, String name, LocalDate dob, String email) {
+        this.id = id;
         this.name = name;
-        this.eamil = eamil;
-        this.date = date;
-        this.age = age;
+        this.dob = dob;
+        this.email = email;
+    }
+
+    public Student(String name, LocalDate dob, String email) {
+        this.name = name;
+        this.dob = dob;
+        this.email = email;
     }
 
     public Long getId() {
@@ -40,28 +57,28 @@ public class Student {
         this.name = name;
     }
 
-    public String getEamil() {
-        return eamil;
+    public LocalDate getDob() {
+        return dob;
     }
 
-    public void setEamil(String eamil) {
-        this.eamil = eamil;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
     }
 
     public Integer getAge() {
-        return age;
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
         this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
@@ -69,9 +86,9 @@ public class Student {
         return "Student{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", eamil='" + eamil + '\'' +
-                ", date=" + date +
+                ", dob=" + dob +
                 ", age=" + age +
+                ", email='" + email + '\'' +
                 '}';
     }
 }
